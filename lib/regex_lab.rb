e313e5f58b32c862859020773f48514e35d52d04
Alyssa Yates <bosslys@icloud.com>
2019-11-22 20:54:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 def starts_with_a_vowel?(word)
      if !/\b[aeiouAEIOU]/
-       true 
+       true
      else
        false
      end 
  end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+
 end
 
 def words_five_letters_long(text)
