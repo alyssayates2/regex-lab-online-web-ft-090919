a7a48eea186bf9b10d90967d3aaf132c526056ee
Alyssa Yates <bosslys@icloud.com>
2019-11-22 21:06:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-     !/\b[aeiouAEIOU]/ == true, !/\b[^aeiouAEIOU]/ == false
+     !/\b[aeiouAEIOU]/ == true !/\b[^aeiouAEIOU]/ == false
  end
 
 def words_starting_with_un_and_ending_with_ing(text)
